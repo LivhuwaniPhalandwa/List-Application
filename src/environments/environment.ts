c29ef1e554a25cf75ca6f56682b2f6d9b8b7d642
LivhuwaniPhalandwa <51481239+LivhuwaniPhalandwa@users.noreply.github.com>
2019-09-27 17:21:49 +0200

    added a text in home page

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
   firebase : {
-    apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+    apiKey: "AIzaSyAHAX4DnYszpY25D_RzbWQMrcZ4FLW_NCg",
+  authDomain: "grocerylist-795b9.firebaseapp.com",
+  databaseURL: "https://grocerylist-795b9.firebaseio.com",
+  projectId: "grocerylist-795b9",
+  storageBucket: "grocerylist-795b9.appspot.com",
+  messagingSenderId: "259198754055",
+  appId: "1:259198754055:web:31f862e7d79c004c3af9a2"
   }
 };
 
