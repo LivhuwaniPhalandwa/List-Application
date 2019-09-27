f8df5dd1b84244ece9080d8e3e5acccfb0119ea5
LivhuwaniPhalandwa <51481239+LivhuwaniPhalandwa@users.noreply.github.com>
2019-09-27 15:54:23 +0200

    Application

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyDDCNXdRSqAOyqo8NOtWkT-1ewuQaO9MMU",
+  authDomain: "bookingapp-3da7d-b3b1d.firebaseapp.com",
+  databaseURL: "https://bookingapp-3da7d-b3b1d.firebaseio.com",
+  projectId: "bookingapp-3da7d",
+  storageBucket: "bookingapp-3da7d.appspot.com",
+  messagingSenderId: "792164457322",
+  appId: "1:792164457322:web:6f1f6670290f1860"
+ }
+};
